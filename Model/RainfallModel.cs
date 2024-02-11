3b12c61d209c6f4722374dddd71d6601a814b107
josefoira23 <josefoira20@gmail.com>
2024-02-11 10:06:10 +0800

    Creation of Model and Test run

---
+++ Model/RainfallModel.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace FlexiSourceExam.Model
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string description { get; set; }
+        public object? content { get; set; }
+    }
+
+
+
+    public class stationReading
+    {
+        [JsonProperty("@context")]
+        public string @context { get; set; }
+        public meta meta { get; set; }
+        public List<stationItem> items { get; set; }
+    }
+
+
+    public class meta
+    {
+        public string publisher { get; set; }
+        public string licence { get; set; }
+        public string documentation { get; set; }
+        public decimal version { get; set; }
+        public string comment { get; set; }
+        public List<string> hasFormat { get; set; }
+        public int limit { get; set; }
+
+
+    }
+    public class stationItem
+    {
+        [JsonProperty("@id")]
+        public string @id { get; set; }
+        public DateTime? datetime { get; set; }
+        public string measure { get; set; }
+        public decimal value { get; set; }
+
+    }
+}
--- obj/Debug/net8.0/FlexiSourceExam.AssemblyInfo.cs
+++ obj/Debug/net8.0/FlexiSourceExam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlexiSourceExam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afadacf2929f1ce8db32a953bd82522222da22e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlexiSourceExam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlexiSourceExam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
